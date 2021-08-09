a659b8aeaf8a598b9381a3f9a8f203992289a83c
Erin Truman <erintruman@Erins-MacBook-Pro.local>
2021-08-09 14:19:09 -0500

    I got the right radio buttons to show up when you pick a facility

--- scripts/Facilities.js
+++ scripts/Facilities.js
@@ -5,22 +5,21 @@ const minerals = getMinerals()
 const facilityQuantities = getFacilityQuantities()
 
 
-
 document.addEventListener(
     "click",
     (event) => { 
         const mineralContainer = document.querySelector(".mineral--options")
         for (const quantity of facilityQuantities) {
             if ( parseInt(event.target.id) === quantity.facilityId) {
-                mineralContainer.innerHTML += mineralButtons()
-                setFacility(parseInt(event.target.value))
+                mineralContainer.innerHTML += MineralButtons()
+                setFacility(parseInt(event.target.id))
             }
         }                  
     }       
 )
 
 
-const mineralButtons = () => {
+export const MineralButtons = () => {
     const state = getOrderBuilder()
     let html ="<ul>"
     for (const quantity of facilityQuantities) {
@@ -33,29 +32,8 @@ const mineralButtons = () => {
     }    
    
     html += "</ul>"
-        return html
+    return html
 }
-
-
-
-
-// // this window alert works
-
-// document.addEventListener(
-//     "click",
-//     (event) => { 
-//         // const mineralContainer = document.querySelector(".mineral--options")
-//         for (const quantity of facilityQuantities) {
-        
-//             if ( parseInt(event.target.id) === quantity.facilityId) {
-//                 const mineral = minerals.find(mineral => mineral.id === quantity.mineralId)
-//                 window.alert(`${mineral.name}.`)
-//             }
-//         }
-//     }                        
-// )
-
-
 
 
 export const FacilityButton = () => {
@@ -73,176 +51,6 @@ export const FacilityButton = () => {
 
     return html
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mineralButtons = () => {
-    
-//     let html ="<ul>"
-//         for (const quantity of facilityQuantities) {
-//             for (const mineral of minerals) {
-//                 if (quantity.mineralId === mineral.id ) {
-//                         html += `<li>
-//                                     <input type ="checkbox" name="mineral" value="${mineral.id}"> ${mineral.name} </input>  
-//                                  </li>`   
-//                 }    
-//             }   
-        
-//         }
-//     html += "</ul>"
-//         return html
-// }
-
-
-
-
-
-
-// document.addEventListener(
-//     "click",
-//     (event) => { 
-//         const mineralContainer = document.querySelector(".mineral--options")
-//         for (const quantity of facilityQuantities) {
-        
-//             if ( parseInt(event.target.id) === quantity.facilityId) {
-
-//                 mineralContainer.innerHTML += mineralButtons()
-        
-//             }
-//         } 
-//     }                       
-// )
-
-
-
-
-
-
-// this windoe alert works
-
-// document.addEventListener(
-//     "click",
-//     (event) => { 
-//         // const mineralContainer = document.querySelector(".mineral--options")
-//         let html ="<ul>"
-//         for (const quantity of facilityQuantities) {
-        
-//             if ( parseInt(event.target.id) === quantity.facilityId) {
-//                 const mineral = minerals.find(mineral => mineral.id === quantity.mineralId)
-//                 html += `<li>
-//                             <input type ="checkbox" name="mineral" value="${mineral.id}"> ${mineral.name} </input>  
-//                         </li>`   
-//             }    
-                   
-//         html += "</ul>"
-//         return html
-//         }
-//     }
-// )
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener(
-//     "click",
-//     (clickEvent) => {
-//         const itemClicked = clickEvent.target
-//         if (itemClicked.id.startsWith("facility")) {
-//             const [, facilityId] = itemClicked.id.split("--")
-
-//             for (const pet of pets) {
-//                 if ( pet.id === parseInt(petId)) {
-
-
-//                     const walker = walkers.find(walker => walker.id === pet.walkerId)
-
-
-//                     window.alert(`${mineral.name}.`)
-//                 }
-//             }
-//         }
-//     }
-// )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener(
-//     "click",
-//     (event) => { 
-//         // const mineralContainer = document.querySelector(".mineral--options")
-//         for (const quantity of facilityQuantities) {
-//                 if ( parseInt(event.target.id) === quantity.facilityId && quantity.mineralId === minerals.id) {
-//                     html += `<li>
-//                     <input type ="checkbox" name="mineral" value="${minerals.id}"> ${minerals.name} </input>  
-//                  </li>`   
-//                 }     
-
-//         }
-//         html += "</ul>"
-//         return html 
-//     } 
-// )
-
-
-// const mineralButtons = () => {
-    
-//     let html ="<ul>"
-//         for (const quantity of facilityQuantities) {
-//             for (const mineral of minerals) {
-//                 if (quantity.mineralId === mineral.id ) {
-//                         html += `<li>
-//                                     <input type ="checkbox" name="mineral" value="${mineral.id}"> ${mineral.name} </input>  
-//                                  </li>`   
-//                 }    
-//             }   
-        
-//         }
-//     html += "</ul>"
-//         return html
-// }
-
-
-
-
-
-
-
-
 
 
 
