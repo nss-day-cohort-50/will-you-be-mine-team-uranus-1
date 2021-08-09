c5297b2890b6acb83d9df07456aafa07b01cfd7c
Matthew Anderson <matt.rich.anderson@gmail.com>
2021-08-09 14:41:13 -0500

    additonal changes

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,5 +1,16 @@
 import { getOrderBuilder } from "./database";
 
 
+export const Orders = () => {
+
+    const orders = getOrderBuilder()
+    let html = "<ul>"
+    const listItems = orders.map(buildOrderListItem)
+    html += listItems.join("")
+    html += "</ul>"
+    return html
+}
+
+
 
 
