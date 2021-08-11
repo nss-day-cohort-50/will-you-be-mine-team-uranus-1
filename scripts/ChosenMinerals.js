d56264a6c5352b7c53e62d825a04f4b718418411
Erin Truman <erintruman@Erins-MacBook-Pro.local>
2021-08-11 10:43:31 -0500

    deleted commented out code before pushing

--- scripts/ChosenMinerals.js
+++ scripts/ChosenMinerals.js
@@ -1,69 +1,10 @@
 import { getOrderBuilder, getMinerals } from "./database.js"
-// import { getFacilities, getFacilityQuantities, getColonyQuantities } from "./database.js"
+
 
 
 
 const minerals = getMinerals()
-// const facilities = getFacilities()
-// const facilityQuantities = getFacilityQuantities()
-// const colonyQuantities = getColonyQuantities()
 
-
-
-// export const ChosenMinerals = () => {
-//     const state = getOrderBuilder()
-//     let html ="<ul>"
-//     const listItems = orders.map(buildOrderListItem)
-    
-//         html += listItems.join("")
-//         html += "</ul>"
-    
-//         return html
-//     }
-
-
-
-// const buildOrderListItem = (order) => {
-
-    
-//     const foundMineral = minerals.find(
-//         (mineral) => {
-//             return mineral.id === order.mineralId
-//         }
-//     )
-
-//     const foundFacility = facilities.find(
-//         (facility) => {
-//             return facility.id === order.facilityId
-//         }
-//     )
-
-//     const foundStyle = styles.find(
-//         (style) => {
-//             return style.id === order.styleId
-//         }
-//     )
-
-//     const foundType = types.find(
-//         (type) => {
-//             return type.id === order.typeId
-//         }
-//     )
-
-//     const totalCost = (foundMetal.price + foundSize.price + foundStyle.price) * foundType.price
-
-     
-//     const costString = totalCost.toLocaleString("en-US", {
-//         style: "currency",
-//         currency: "USD"
-//     })
-    
-//     return `<li>
-//         Order #${order.id} cost ${costString}
-//     </li>`
-
-
-// }
 
 
 // original code
@@ -85,16 +26,3 @@ export const ChosenMinerals = () => {
 
 
 
-
-// export const ChosenMinerals = () => {
-//     const state = getOrderBuilder()
-//     const mineralFacilityObjects = mineralFacilities.filter(mineralFacilityObject => state.facilityId === mineralFacilityObject.facilityId)
-//     const arrayMatchedItems = mineralFacilityObjects.map(mineralFacilityObject => {
-//     const MatchedMineralObject = minerals.find(mineral => mineral.id === mineralFacilityObject.mineralId)    
-//         return `<div>
-//              <input type="radio">${mineralFacilityObject.quantity} ${MatchedMineralObject.name}
-//         </div>` 
-//     }).join("")
-
-//     return arrayMatchedItems
-// }
